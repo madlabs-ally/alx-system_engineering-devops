273a02f037aac747a9c2aa4d9180982c5a4ae8c7
madlabs-ally <madalitsosimbeye173@gmail.com>
2023-04-24 14:38:37 -0700

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,38 @@
-#!/usr/bin/python3
-"""This module returns information about an employee's TODO list progress."""
-
+#!/usr/bin/env python3
 
 import requests
 import json
-from sys import argv
+import sys
 
 
-def employee_todo_progress():
-    response = requests.get('https://jsonplaceholder.typicode.com/todos?',
-                            params={'userId': argv[1]})
-    if response.status_code != 200:
-        print(f"Error: {response.status_code}")
-        return
+def get_todos(user_id):
+    try:
+        response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={user_id}")
+        response.raise_for_status()  # raise an exception if the request failed
+        todos = response.json()
+        return todos
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return []
 
-    todos = response.json()
-    completed_tasks = []
-    for todo in todos:
-        if todo['completed']:
-            completed_tasks.append(todo)
 
-    user_id = todos[0]['userId']
+def get_username(user_id):
+    try:
+        response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
+        response.raise_for_status()
+        user = response.json()
+        return user['username']
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+
+def filter_completed_tasks(todos):
+    return [todo for todo in todos if todo['completed']]
+
+
+def generate_tasks_dict(user_id, todos, username):
     tasks = []
-    username = requests.get('https://jsonplaceholder.typicode.com/users',
-                            params={'id': user_id}).json()[0]['username']
     for todo in todos:
         tasks.append({
             "task": todo['title'],
@@ -31,16 +40,35 @@ def employee_todo_progress():
             "username": username
         })
 
-    data = {
-        str(user_id): tasks
-    }
+    return { str(user_id): tasks }
 
-    file_name = f"{user_id}.json"
-    with open(file_name, "w") as f:
-        json.dump(data, f)
 
-    print(f"JSON file {file_name} has been created successfully.")
+def write_tasks_to_file(tasks_dict, user_id):
+    filename = f"{user_id}.json"
+    with open(filename, "w") as f:
+        json.dump(tasks_dict, f)
+    print(f"JSON file {filename} has been created successfully.")
+
+
+def main(user_id):
+    todos = get_todos(user_id)
+    if not todos:
+        return
+
+    username = get_username(user_id)
+    if not username:
+        return
+
+    completed_tasks = filter_completed_tasks(todos)
+    tasks_dict = generate_tasks_dict(user_id, todos, username)
+    write_tasks_to_file(tasks_dict, user_id)
 
 
 if __name__ == "__main__":
-    employee_todo_progress()
+    if len(sys.argv) != 2:
+        print("Usage: python3 todo.py <user_id>")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    main(user_id)
+
